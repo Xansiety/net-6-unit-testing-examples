6a92cbf5161b01af27ea106458d947878ac2b8b8
xansiety <ferando543@outlook.com>
2023-06-25 17:48:51 -0600

    test(NUnit): First example qith MOQ Framework - Mocking classes

--- TestingLibraryNUnit.test/CuentaBancariaNUnitTest.cs
+++ TestingLibraryNUnit.test/CuentaBancariaNUnitTest.cs
@@ -1,11 +1,13 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 
 namespace TestingLibrary
 {
     [TestFixture]
     public class CuentaBancariaNUnitTest
     {
-        private CuentaBancaria cuentaBancaria;
+        //private CuentaBancaria cuentaBancaria;
+        private CuentaBancaria cuenta;
 
         [SetUp]
         public void Setup()
@@ -13,12 +15,13 @@ namespace TestingLibrary
             // Así pasamos clases al constructor pero no se debe realizar esto
             //cuentaBancaria = new CuentaBancaria(new LoggerGeneral());
             // lo que se debe de hacer es crear una clase falsa
-            cuentaBancaria = new CuentaBancaria(new LoggerGeneralFake());
+            //cuentaBancaria = new CuentaBancaria(new LoggerGeneralFake());
         }
 
         [Test]
         public void CuentaBancaria_Deposito_DebeRetornarTrue()
         {
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(new LoggerGeneralFake());
             var resultado = cuentaBancaria.Deposito(100);
             Assert.IsTrue(resultado);
         }
@@ -26,11 +29,26 @@ namespace TestingLibrary
         [Test]
         public void BalanceCuenta_ElBalanceDebeSerIgualAlDeposito()
         {
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(new LoggerGeneralFake());
             var deposito = 100;
             cuentaBancaria.Deposito(deposito);
             var resultado = cuentaBancaria.GetBalance();
             Assert.AreEqual(deposito, resultado);
             Assert.That(resultado, Is.EqualTo(deposito));
         }
+
+
+        // MOQ - Implementation 
+        [Test]
+        public void CuentaBancariaMocking_Deposito_DebeRetornarTrue()
+        {
+            var mocking = new Mock<ILoggerGeneral>();
+            // .Object es para obtener el objeto y su instancia
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(mocking.Object); // Se pasa el objeto
+
+            var resultado = cuentaBancaria.Deposito(100);
+            Assert.IsTrue(resultado);
+        }
+
     }
 }
