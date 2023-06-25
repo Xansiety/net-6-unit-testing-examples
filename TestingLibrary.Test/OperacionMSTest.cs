c54c46e0fcf1fc2e3cb854d23b8dbc0bda11d2a0
xansiety <ferando543@outlook.com>
2023-06-25 14:02:37 -0600

    test(MSTest): First Unit Test

---
+++ TestingLibrary.Test/OperacionMSTest.cs
@@ -0,0 +1,25 @@
+﻿// Ignore Spelling: Numeros Operacion
+using TestingLibrary;
+
+namespace TestingLibraryMS.Test
+{
+    [TestClass]
+    public class OperacionMSTest
+    {
+        [TestMethod]
+        public void SumarNumeros_DebeRealizarLaSumaDeDosNumeros()
+        {
+            // Arrange -> Inicializar variables o componentes que ejecutaran el test
+            Operacion op = new Operacion();
+            int numero1 = 2;
+            int numero2 = 2;
+            var resultadoEsperado = numero1 + numero2;
+
+            // Act
+            int resultado = op.SumarNumeros(numero1: numero1, numero2: numero2);
+
+            // Assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+    }
+}
