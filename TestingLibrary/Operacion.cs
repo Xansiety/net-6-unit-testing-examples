3a9a240cc833cff573ce187e64a0e3a814706529
xansiety <ferando543@outlook.com>
2023-06-25 14:29:23 -0600

    test(NUnit): First NUnit Tests

--- TestingLibrary/Operacion.cs
+++ TestingLibrary/Operacion.cs
@@ -1,10 +1,17 @@
-﻿namespace TestingLibrary
+﻿// Ignore Spelling: Numeros Operacion
+
+namespace TestingLibrary
 {
     public class Operacion
     {
         public int SumarNumeros(int numero1, int numero2)
         {
             return numero1 + numero2;
+        }
+
+        public bool IsPairValue(int number)
+        {
+            return number % 2 == 0;
         }
     }
 }
